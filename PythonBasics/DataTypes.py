1234611807976826931dc835676a386bee80c984
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-11-15 14:27:31 +0530

    datatypes

---
+++ PythonBasics/DataTypes.py
@@ -0,0 +1,10 @@
+# Demonstrating different data types in Python
+
+# Python datatypes include:
+    # Numeric types: int, float, complex
+    # Sequence types: list, tuple, range
+    # Text type: str
+    # Set types: set, frozenset
+    # Mapping type: dict
+    # Boolean type: bool
+
