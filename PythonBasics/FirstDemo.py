a5b4ec27e35a8dba15bd4e3a4b10b638a9392789
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-11-15 14:01:40 +0530

    first demo

---
+++ PythonBasics/FirstDemo.py
@@ -0,0 +1,17 @@
+print("Hello, this is a basic Python script for Pytest demonstration.")
+
+# comments can be added using the hash symbol (#)
+
+# Variables
+a = 20
+print(a)
+
+string = "Hello, Pytest!"
+print(string)
+
+b, c, d = 5, 10.5, "Test"
+print(b)
+print(c)
+print(d)
+
+
