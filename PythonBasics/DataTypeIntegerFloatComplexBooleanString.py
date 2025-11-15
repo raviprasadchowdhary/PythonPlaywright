4c945d879bca6cbb5e46a210b5369ce4c576478b
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-11-15 14:28:12 +0530

    integer Float Boolean complex string

---
+++ PythonBasics/DataTypeIntegerFloatComplexBooleanString.py
@@ -0,0 +1,21 @@
+a = 3                   # Integer
+b = 4.5                 # Float
+c = "Hello, World!"     # String
+d = True                # Boolean
+e = 3 + 4j              # Complex Number
+
+print("Integer:", a)
+print("Float:", b)
+print("String:", c)
+print("Boolean:", d)
+print("Complex Number:", e)
+
+print("{} {}".format("Value of Integer is:", a))
+
+print(f"Value of boolean is: {d} and value of float is: {b}")
+
+print("Type of a is:", type(a))
+print("Type of b is: ", type(b))
+print("Type of c is: ", type(c))
+print("type of d is: ", type(d))
+print("type of e is: ", type(e))
