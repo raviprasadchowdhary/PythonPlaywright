c0b99d6a4375bcac29f46c2b4d9a5f894825d4fa
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-11-30 13:22:43 +0530

    test_core_locators_getByLabelAndRole

--- Playwright/test_coreLocators.py
+++ Playwright/test_coreLocators.py
@@ -6,6 +6,9 @@ def test_core_locators_getByLabelAndRole(page:Page):
     page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     # labels
     page.get_by_label("Username:").fill("rahulshettyacademy")
-    page.get_by_label("Password:").fill("123")
+    page.get_by_label("Password:").fill("learning")
+    # roles
     page.get_by_role("combobox").select_option("consult")
-    time.sleep(2)
+    # print(page.get_by_role("button"))
+    page.get_by_role("button", name="Sign In").click()
+    time.sleep(10)
