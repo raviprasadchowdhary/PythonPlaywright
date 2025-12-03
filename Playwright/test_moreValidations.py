772bacd56381406b680b0246dac566548c515f42
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-03 23:09:10 +0530

    test_alerts_dialog

--- Playwright/test_moreValidations.py
+++ Playwright/test_moreValidations.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import Page, expect
 
 
-def test_UILocators(page: Page):
+def test_getByPlaceHolder(page: Page):
     page.goto("https://rahulshettyacademy.com/AutomationPractice/")
     # placeholder locator
     # assertion to verify the element is visible
@@ -9,3 +9,9 @@ def test_UILocators(page: Page):
     # click hide button
     page.get_by_role("button", name="Hide").click()
     expect(page.get_by_placeholder("Hide/Show Example")).to_be_hidden()
+
+def test_alerts_dialog(page:Page):
+    page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+    # handle alert
+    page.on("dialog", lambda dialog: dialog.accept())
+    page.get_by_role("button", name="Confirm").click()
