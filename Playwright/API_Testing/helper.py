c26474ec66b77e689566f70d7f83c4faf023e84c
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-07 22:44:39 +0530

    refactor

--- Playwright/API_Testing/helper.py
+++ Playwright/API_Testing/helper.py
@@ -5,18 +5,21 @@ from playwright.sync_api import Playwright
 def apiRequestContext(playwright: Playwright):
     return  playwright.request.new_context(base_url="https://rahulshettyacademy.com", ignore_https_errors=True)
 
+# here we are creating a reusable function to get the auth token
 def getAuthToken(playwright):
     api_context = apiRequestContext(playwright)
     response = api_context.post(url="/api/ecom/auth/login"
                                 , data=getAuthTokenRequestBody)
     return response.json()["token"]
 
+# here we are creating a reusable function to add Zara Coat 3 to cart
 def addToCartZaraCoat3(playwright):
     return apiRequestContext(playwright).post("/api/ecom/user/add-to-cart"
                                                   , data = addToCartRequestBodyZaraCoat3
                                                   , headers={"Authorization": getAuthToken(playwright)}
                                               )
 
+# here we are creating a reusable function to create order for Zara Coat 3
 def createOrder(playwright):
     return apiRequestContext(playwright).post(url= "/api/ecom/order/create-order"
                                        , headers={"Authorization": getAuthToken(playwright)}
@@ -27,11 +30,13 @@ userEmail = "RahulKumar@gmail.com"
 userPassword = "Rahul@123"
 
 # ************************************************Request Body*********************************************************
+# here we are creating reusable request body for getting auth token
 getAuthTokenRequestBody = {
     "userEmail": userEmail,
     "userPassword": userPassword
     }
 
+# here we are creating reusable request body for adding Zara Coat 3 to cart
 addToCartRequestBodyZaraCoat3 = {
     "_id": "693401c232ed865871222818",
     "product": {
@@ -51,6 +56,7 @@ addToCartRequestBodyZaraCoat3 = {
     }
 }
 
+# here we are creating reusable request body for creating order for Zara Coat 3
 createOrderRequestBodyZaraCoat3 = {
     "orders": [
         {
