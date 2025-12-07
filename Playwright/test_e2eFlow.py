acc8889caf7bb84ef1ad7978c8fe587883a2ec19
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-07 13:01:34 +0530

    refactor

--- Playwright/test_e2eFlow.py
+++ Playwright/test_e2eFlow.py
@@ -4,15 +4,21 @@ from utils.apiBase import APIUtils
 
 
 def test_createOrderAndVerify(playwright: Playwright):
+    #setup
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
 
+    # login
     page.goto("https://rahulshettyacademy.com/client")
     page.get_by_placeholder("email@example.com").fill("RahulKumar@gmail.com")
     page.get_by_placeholder("enter your passsword").fill("Rahul@123")
     page.get_by_role("button", name="Login").click()
 
+    # create order via API and get orderId
     orderId = APIUtils.createOrder(playwright)
+    print(f"Order ID from API is: {orderId}")
+
+    # navigate to Orders page and verify placed orderId is present in the Orders list
     page.get_by_role("button", name="ORDERS").click()
     assert orderId == page.locator("tbody tr th").nth(0).text_content()
