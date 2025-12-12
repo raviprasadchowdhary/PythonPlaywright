a082838271fa73838afc2c730533056ff307ab55
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-12 12:57:11 +0530

    test_injectSessionCookiesIntoBrowserAtRunTime

--- Playwright/NetworkInterception/test_injectSessionCookiesIntoBrowserAtRunTime.py
+++ Playwright/NetworkInterception/test_injectSessionCookiesIntoBrowserAtRunTime.py
@@ -1,6 +1,22 @@
+import time
+
+from playwright.sync_api import Playwright
+
+from utils.apiBase import APIUtils
+
 
 def test_injectSessionCookiesIntoBrowserAtRunTime(playwright: Playwright):
-    browser = playwright.chromium.laumch(headless=False)
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://rahuleshettyacademy.com/")
+
+    # here we are getting the token by calling the API directly and then injecting that token into the browser's local storage
+    # before navigating to the page. This way, we bypass the login process.
+    # this is useful when we want to test pages that require authentication without going through the login UI.
+    apiUtils = APIUtils()
+    token = apiUtils.getToken(playwright)
+    print(f"Token is: {token}")
+    page.add_init_script(f"""localStorage.setItem("token", "{token}")""")
+
+    page.goto("https://rahulshettyacademy.com/client/#/dashboard/dash")
+
