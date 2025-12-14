af6e99d59d67bdbef1368845eedcd69e1bd85e05
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-14 08:05:47 +0530

    refactor test_injectSessionCookiesIntoBrowserAtRunTime

--- Playwright/NetworkInterception/test_injectSessionCookiesIntoBrowserAtRunTime.py
+++ Playwright/NetworkInterception/test_injectSessionCookiesIntoBrowserAtRunTime.py
@@ -12,10 +12,11 @@ def test_injectSessionCookiesIntoBrowserAtRunTime(playwright: Playwright):
     # before navigating to the page. This way, we bypass the login process.
     # this is useful when we want to test pages that require authentication without going through the login UI.
     api_utils = APIUtils()
-    token = api_utils.getToken(playwright)
+    token = api_utils.getToken(playwright, userCredentials={"username":"RahulChowdhary@gmail.com", "password": "Rahul@123"})
     print(f"Token is: {token}")
     page.add_init_script(f"""localStorage.setItem("token", "{token}")""")
 
     page.goto("https://rahulshettyacademy.com/client/#/dashboard/dash")
+    api_utils.createOrder(playwright, userCredentials={"username":"RahulChowdhary@gmail.com", "password": "Rahul@123"})
     page.get_by_role("button", name="ORDERS").first.click()
     expect(page.get_by_text("Your Orders")).to_be_visible()
