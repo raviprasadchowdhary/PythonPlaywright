987045a9b29f6f873193c358e5ea0e4cc9751be1
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-14 08:32:23 +0530

    login page

---
+++ Playwright/Framework/PageObjects/loginPage.py
@@ -0,0 +1,7 @@
+class LoginPage:
+    def __init__(self, page):
+        self.page = page
+
+    # @staticmethod
+    def navigate(self):
+        self.page.goto("https://rahulshettyacademy.com/client")
