1c34aec5832d2f869993ac96e4021ae2aedc5832
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-15 21:34:16 +0530

    added comments

--- Playwright/Framework/Tests/test_e2eFlowHybridFramework.py
+++ Playwright/Framework/Tests/test_e2eFlowHybridFramework.py
@@ -16,15 +16,17 @@ print(f"Test data loaded from json file: {test_data}")
 
 @pytest.mark.parametrize("test_credentials_list", test_credentials)
 def test_e2eTest_createOrderAndVerify(playwright: Playwright, test_credentials_list, page):
+    # here we will create order by API call
     orderId = APIUtils.createOrder(playwright, userCredentials=test_credentials_list)
 
+    # here we will verify orderId in UI
     common = Common(playwright)
     page = common.create_browser_context("chrome", "False")
-
     loginPage = LoginPage(page)
     loginPage.navigate()
     dashboardPage = loginPage.login(test_credentials_list)
     orderDetailsPage = dashboardPage.click_order_button()
     orderDetailsPage.clickViewByOrderId(orderId)
 
+    # here we will add assertion to verify orderId in UI
     expect(page.locator(".tagline")).to_have_text("Thank you for Shopping With Us")
