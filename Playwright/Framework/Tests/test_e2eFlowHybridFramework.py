6ddbc75fe857d1aeda687f300f74a93d5e3f7401
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-12-16 22:14:13 +0530

    using browser instance from addoption conftest

--- Playwright/Framework/Tests/test_e2eFlowHybridFramework.py
+++ Playwright/Framework/Tests/test_e2eFlowHybridFramework.py
@@ -15,18 +15,16 @@ print(f"Test data loaded from json file: {test_data}")
 
 
 @pytest.mark.parametrize("test_credentials_list", test_credentials)
-def test_e2eTest_createOrderAndVerify(playwright: Playwright, test_credentials_list, page):
+def test_e2eTest_createOrderAndVerify(playwright: Playwright, browser_instance, test_credentials_list):
     # here we will create order by API call
     orderId = APIUtils.createOrder(playwright, userCredentials=test_credentials_list)
 
-    # here we will verify orderId in UI
-    common = Common(playwright)
-    page = common.create_browser_context("chrome", "False")
-    loginPage = LoginPage(page)
+    # here we will log in to the application and verify orderId in UI
+    loginPage = LoginPage(browser_instance)
     loginPage.navigate()
     dashboardPage = loginPage.login(test_credentials_list)
     orderDetailsPage = dashboardPage.click_order_button()
     orderDetailsPage.clickViewByOrderId(orderId)
 
     # here we will add assertion to verify orderId in UI
-    expect(page.locator(".tagline")).to_have_text("Thank you for Shopping With Us")
+    expect(browser_instance.locator(".tagline")).to_have_text("Thank you for Shopping With Us")
